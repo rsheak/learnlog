051a04b138fca4fbf26b87ed9d6d4097ce9ce33c
rsheak <rshea001@campus.goldsmiths.ac.uk>
2024-12-13 01:05:59 +0000

    added new routes

--- routes/main.js
+++ routes/main.js
@@ -75,4 +75,9 @@ router.post("/add-log", (req, res) => {
   });
 });
 
+app.listen(3000, '0.0.0.0', () => {
+  console.log('LearnLog app is running at http://0.0.0.0:3000');
+  console.log('Connected to the MySQL database.');
+});
+
 module.exports = router;
